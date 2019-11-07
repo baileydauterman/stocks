60eade77387bd973a3f96973a01fb8a8d9841222
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2019-11-07 12:11:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ from terminaltables import AsciiTable
 from datetime import datetime
 
 def get_stock(ticker):
+    if isinstance(ticker, list):
+        for x in ticker:
+            current_stock_price(x)
+            current_stock_summary(x)
+
+    else:
+        current_stock_summary(ticker)
+
+
+def get_valuation(ticker):
+    if isinstance(ticker, list):
+        for x in ticker:
+            current_stock_statistics(x)
+    else:
+        current_stock_statistics(ticker)
+
+def get_wsj_movers():
+    print("Work in progress")
+
+def get_wsj_high_low():
+    print("Work in progress")
+
+def get_wsj_sectors():
+    print("Work in progress")
+
+def current_stock_price(ticker):
     url = "https://finance.yahoo.com/quote/{}".format(ticker)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -16,7 +42,19 @@ def get_stock(ticker):
     price = str(span[13]).split(">")[1].split("<")[0]
     change = str(span[14]).split(">")[1].split("<")[0]
     print(f"\n\n{ticker} \nPrice: {price} \nChange:{change}\n")
-    
+
+def current_stock_summary(ticker):
+    url = "https://finance.yahoo.com/quote/{}".format(ticker)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    span = soup.findAll('span')
+    td = soup.find_all('td')
+    now = datetime.now()
+
+    price = str(span[13]).split(">")[1].split("<")[0]
+    change = str(span[14]).split(">")[1].split("<")[0]
+    print(f"\n\n{ticker} \nPrice: {price} \nChange:{change}\n")
+
     if now.hour >= 16:
         prev_close = str(span[23]).split(">")[1].split("<")[0]
         prev_close_price = str(span[24]).split(">")[1].split("<")[0]
@@ -56,19 +94,19 @@ def get_stock(ticker):
         eps_ratio = str(span[44]).split(">")[1].split("<")[0]
 
         table_date = [
-            [ticker, 'Summary'],
-            [prev_close, prev_close_price],
-            [open_,open_price],
-            [bid, bid_price],
-            [ask, ask_price],
-            [day_range, day_range_price],
-            [week_range, week_range_price],
-            [volume, volume_price],
-            [avg_volume, avg_volume_price],
-            [market_cap, market_cap_price],
-            [beta, beat_price],
-            [pe, pe_ratio],
-            [eps, eps_ratio]
+        [ticker, 'Summary'],
+        [prev_close, prev_close_price],
+        [open_,open_price],
+        [bid, bid_price],
+        [ask, ask_price],
+        [day_range, day_range_price],
+        [week_range, week_range_price],
+        [volume, volume_price],
+        [avg_volume, avg_volume_price],
+        [market_cap, market_cap_price],
+        [beta, beat_price],
+        [pe, pe_ratio],
+        [eps, eps_ratio]
         ]
         table = AsciiTable(table_date)
         print(table.table)
@@ -110,25 +148,25 @@ def get_stock(ticker):
         eps_ratio = str(span[41]).split(">")[1].split("<")[0]
 
         table_date = [
-            [ticker, 'Summary'],
-            [prev_close, prev_close_price],
-            [open_,open_price],
-            [bid, bid_price],
-            [ask, ask_price],
-            [day_range, day_range_price],
-            [week_range, week_range_price],
-            [volume, volume_price],
-            [avg_volume, avg_volume_price],
-            [market_cap, market_cap_price],
-            [beta, beat_price],
-            [pe, pe_ratio],
-            [eps, eps_ratio]
+        [ticker, 'Summary'],
+        [prev_close, prev_close_price],
+        [open_,open_price],
+        [bid, bid_price],
+        [ask, ask_price],
+        [day_range, day_range_price],
+        [week_range, week_range_price],
+        [volume, volume_price],
+        [avg_volume, avg_volume_price],
+        [market_cap, market_cap_price],
+        [beta, beat_price],
+        [pe, pe_ratio],
+        [eps, eps_ratio]
         ]
         table = AsciiTable(table_date)
         print(table.table)
 
 
-def get_valuation(ticker):
+def current_stock_statistics(ticker):
     url = "https://finance.yahoo.com/quote/{}/key-statistics?p={}".format(ticker, ticker)
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -137,6 +175,48 @@ def get_valuation(ticker):
     now = datetime.now()
     
     if now.hour >= 16:
+        market_cap = str(span[15]).split(">")[1].split("<")[0]
+        market_cap_price = str(td[1]).split(">")[1].split("<")[0]
+
+        enterprise_value = str(span[16]).split(">")[1].split("<")[0]
+        enterprise_value_price = str(td[3]).split(">")[1].split("<")[0]
+
+        trailing_pe = str(span[17]).split(">")[1].split("<")[0]
+        trailing_pe_num = str(td[5]).split(">")[1].split("<")[0]
+
+        forward_pe = str(span[18]).split(">")[1].split("<")[0]
+        forward_pe_num = str(td[7]).split(">")[1].split("<")[0]
+
+        peg = str(span[19]).split(">")[1].split("<")[0]
+        peg_ratio = str(td[9]).split(">")[1].split("<")[0]
+
+        ps = str(span[20]).split(">")[1].split("<")[0]
+        ps_ratio = str(td[11]).split(">")[1].split("<")[0]
+
+        pb = str(span[21]).split(">")[1].split("<")[0]
+        pb_ratio = str(td[13]).split(">")[1].split("<")[0]
+
+        ev = str(span[22]).split(">")[1].split("<")[0]
+        evr = str(td[15]).split(">")[1].split("<")[0]
+
+        ebitda = str(span[23]).split(">")[1].split("<")[0]
+        evebitda = str(td[17]).split(">")[1].split("<")[0]
+
+        table_date = [
+            [ticker, 'Valuation'],
+            [market_cap, market_cap_price],
+            [enterprise_value,enterprise_value_price],
+            [trailing_pe, trailing_pe_num],
+            [forward_pe, forward_pe_num],
+            [peg, peg_ratio],
+            [ps, ps_ratio],
+            [pb, pb_ratio],
+            [ev, evr],
+            [ebitda, evebitda]
+        ]
+        table = AsciiTable(table_date)
+        print(table.table)
+    else: 
         market_cap = str(span[15]).split(">")[1].split("<")[0]
         market_cap_price = str(td[1]).split(">")[1].split("<")[0]
 
